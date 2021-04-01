98b7bf1166aa0f713964be8c4fb0adc7c3776171
Xavier <carlosf.s.x@hotmail.com>
2021-04-01 16:17:11 -0300

    Estrutura do projeto

---
+++ src/main/java/com/unisinos/mse/MseApplication.java
@@ -0,0 +1,13 @@
+package com.unisinos.mse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MseApplication.class, args);
+	}
+
+}
