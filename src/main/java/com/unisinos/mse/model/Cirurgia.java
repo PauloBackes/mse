597a8b26f24cd21474eeca5fd862b0b55861bb63
Carlos Fernando Santos Xavier <carlosf@sft.inf.br>
2021-05-06 23:41:31 -0300

    Configuração do mongoDB e remoção do postgres

--- src/main/java/com/unisinos/mse/model/Cirurgia.java
+++ src/main/java/com/unisinos/mse/model/Cirurgia.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Document(collection = "cirurgia")
 public class Cirurgia {
-    private int id;
+    @Id
+    private String id;
     private Equipamento equi;
     private String nome;
     private String descricao;
