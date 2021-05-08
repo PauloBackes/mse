b4564fb5112bfd4486d7506b59cb228d4f2accda
Carlos Fernando Santos Xavier <carlosf@sft.inf.br>
2021-05-07 23:34:53 -0300

    Revert "Merge pull request #1 from PauloBackes/reestrutura"
    This reverts commit 7847ac0ff03aa12d89a87f4152a1bb8b2a768fd2, reversing
    changes made to 85f8553eb50456480f8c3bc53fcca3d1c6f6afd6.

--- src/main/java/com/unisinos/mse/MseApplication.java
+++ src/main/java/com/unisinos/mse/MseApplication.java
@@ -2,14 +2,12 @@ package com.unisinos.mse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class MseApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MseApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MseApplication.class, args);
+	}
 
 }
--- src/main/java/com/unisinos/mse/model/Cirurgia.java
+++ src/main/java/com/unisinos/mse/model/Cirurgia.java
@@ -4,17 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
-@Document(collection = "cirurgia")
 public class Cirurgia {
-    @Id
-    private String id;
+    private int id;
     private Equipamento equi;
     private String nome;
     private String descricao;
