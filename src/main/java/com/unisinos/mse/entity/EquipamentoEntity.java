5edcf3685ee955b96d3a160dfa32babfc7a61449
Carlos Fernando Santos Xavier <carlosf@sft.inf.br>
2021-06-21 22:35:10 -0300

    Configuração

---
+++ src/main/java/com/unisinos/mse/entity/EquipamentoEntity.java
@@ -0,0 +1,17 @@
+package com.unisinos.mse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class EquipamentoEntity {
+    private String descricao;
+    private String patrimonio;
+    private String codigo;
+    private Boolean validado;
+}
---
+++ src/main/java/com/unisinos/mse/entity/MaterialEntity.java
@@ -0,0 +1,17 @@
+package com.unisinos.mse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class MaterialEntity {
+    private String descricao;
+    private String codigo;
+    private Integer quantidade;
+    private Boolean validado;
+}
